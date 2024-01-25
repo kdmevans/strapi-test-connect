73d866e1aed67745cdb6df15327c31b2844cea96
kdmevans <maina@kdm.ke>
2024-01-25 06:53:01 +0300

    initial commit

---
+++ test/database.spec.js
@@ -0,0 +1,22 @@
+import { Client } from "pg";
+import { expect, test } from "vitest";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+test("Test database connectivity", async () => {
+  const client = new Client({
+    user: process.env.DATABASE_USERNAME,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: 5432,
+  });
+
+  await client.connect();
+  const res = await client.query("SELECT * FROM blogs");
+  console.table(res.rows);
+  await client.end();
+
+  expect(res.rows.length).toBeGreaterThan(0);
+});
